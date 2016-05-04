8c2fe09d72f46e150f1ce3c016907528e536d952
masarakki <masaki182@gmail.com>
2016-05-04 15:09:15 +0900

    chrome extension

--- src/popup.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $("#signin").click(function() {
-    chrome.runtime.sendMessage({
-      action: "sign_in"
-    }, function(result) {
-       });
-    return false;
-  });
-});
