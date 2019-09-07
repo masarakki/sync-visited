dff3f31914498444f27b8fcde892cfb61bc7b8a6
masarakki <masaki182@gmail.com>
2019-09-08 00:51:01 +0900

    partialize-output-dir

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const dotenv = require('dotenv');
 const webpack = require('webpack');
+const JsonPlugin = require('generate-json-webpack-plugin');
 
 if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = 'development';
@@ -8,10 +9,13 @@ if (!process.env.NODE_ENV) {
 dotenv.config({ path: '.env' });
 dotenv.config({ path: '.env.aws' });
 
+const isProd = process.env.NODE_ENV === 'production';
+const outputDir = isProd ? path.resolve(__dirname, 'prod') : path.resolve(__dirname, 'dev');
+
 module.exports = {
   mode: process.env.NODE_ENV,
   entry: {
-    background: path.resolve(__dirname, 'app/scripts.babel/background.js'),
+    'scripts/background.js': path.resolve(__dirname, 'app/scripts.babel/background.js'),
   },
   module: {
     rules: [
@@ -25,13 +29,22 @@ module.exports = {
     ],
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'app/scripts/'),
+    filename: '[name]',
+    path: outputDir,
   },
   plugins: [
     new webpack.EnvironmentPlugin({
-      AWS_TOPIC_ARN: process.env.NODE_ENV === 'production' ? process.env.AWS_PROD_TOPIC_ARN : process.env.AWS_DEV_TOPIC_ARN,
+      AWS_TOPIC_ARN: isProd ? process.env.AWS_PROD_TOPIC_ARN : process.env.AWS_DEV_TOPIC_ARN,
     }),
+    new JsonPlugin('manifest.json', {
+      manifest_version: 2,
+      version: '2.0.0',
+      name: isProd ? 'sync-visted' : 'sync-visited (dev)',
+      description: 'sync visited urls',
+      background: { scripts: ['scripts/background.js'] },
+      permissions: ['gcm', 'background', 'history', 'storage'],
+      browser_action: { default_title: 'sync-visted' },
+    }, null, 2),
   ],
   devtool: 'source-map',
 };
