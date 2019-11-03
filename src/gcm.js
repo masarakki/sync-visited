a04e3efbda17026dff1599080a0dda0c8d8cbe1d
masarakki <masaki182@gmail.com>
2019-11-04 07:46:16 +0900

    output-gcm-info

--- src/gcm.js
+++ src/gcm.js
@@ -1,6 +1,6 @@
 import { load } from './storage';
 
-export const senderId = process.env.GCM_SENDER_ID;
+export const gcmSenderId = process.env.GCM_SENDER_ID;
 export const getGcmDeviceId = () => load('gcmDeviceId', () => new Promise((resolve) => {
-  chrome.gcm.register([senderId], resolve);
-}));
+  chrome.gcm.register([gcmSenderId], resolve);
+})).then((gcmDeviceId) => ({ gcmSenderId, gcmDeviceId }));
